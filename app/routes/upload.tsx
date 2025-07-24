430716b9712e75cd2e15c419d29703dc9c4bdfc7
Nischal <nyyscal541556@gmail.com>
2025-07-24 11:20:34 +0545

    Feedback Page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -51,6 +51,7 @@ const Upload = () => {
   await kv.set(`resume:${uuid}`, JSON.stringify(data))
   setStatusText("Saving data...")
   console.log(data)
+  navigate(`/resume/${uuid}`)
   }
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>)=> {
